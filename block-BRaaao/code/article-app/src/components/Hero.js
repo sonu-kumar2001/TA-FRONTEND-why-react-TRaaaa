57d7e16f026890aadbcca72660ef39763eab03b4
sonu <sonurocckkerr@gmail.com>
2021-01-22 02:17:22 +0530

    completed news app

---
+++ block-BRaaao/code/article-app/src/components/Hero.js
@@ -0,0 +1,12 @@
+
+function Hero() {
+    return      <section class="hero">
+                    <div class="container1 flex-centre">
+                        <h1 class="hero-heading uppercase">Article on news</h1>
+                        <p class="hero-text">Best article on news app</p>
+                        <a class="btn primary-btn" href="##">Explore</a>
+                    </div>
+                </section>
+}
+
+export default Hero;
