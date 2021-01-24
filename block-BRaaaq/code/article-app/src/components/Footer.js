70000c2fd3702b1aba4d756f73c624f2061f2ca1
sonu <sonurocckkerr@gmail.com>
2021-01-24 13:24:52 +0530

    proptypes not working

---
+++ block-BRaaaq/code/article-app/src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+    return <footer>
+    <div className="container1">
+        <nav>
+            <ul className="nav-foot">
+                <li><a href="##">About</a></li>
+                <li><a href="##">Term of Use</a></li>
+                <li><a href="##">Privacy Policy</a></li>
+                <li><a href="##">Contact Us</a></li>
+            </ul>
+        </nav>
+        <p className="text-centre footer-para">All rights reserved</p>
+    </div>
+</footer>
+}
+
+export default Footer;
