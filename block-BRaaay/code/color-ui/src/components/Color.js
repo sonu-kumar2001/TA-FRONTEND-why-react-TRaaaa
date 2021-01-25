a039801b98444a5e8aaba4bca0911874d7ea0d2d
sonu <sonurocckkerr@gmail.com>
2021-01-25 10:06:44 +0530

    initial commit

---
+++ block-BRaaay/code/color-ui/src/components/Color.js
@@ -0,0 +1,13 @@
+import data from "../colors.json"
+
+function Color() {
+    return <section className="container">
+        {
+            [data].map((color)=> {
+                return ;
+            })
+        }
+    </section>
+}
+
+export default Color;
